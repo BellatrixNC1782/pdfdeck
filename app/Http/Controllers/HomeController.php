2cb13eaa29232c828be99f5e82dc76da19f9a996
dharmesh87 <135203983+dharmesh3699@users.noreply.github.com>
2025-10-27 13:21:09 +0530

    Create required migration for web visitor entry add & implement on home page.

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,10 +4,35 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use setasign\Fpdi\Tcpdf\Fpdi;
+use App\Models\ProfileVisitor;
+use App\Models\Appurls;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Str;
 
 class HomeController extends Controller {
 
-    public function index() {
+    public function index(Request $request) {
+        $response = Http::get('http://ip-api.com/json/' . $request->ip());
+        if ($response->ok()) {
+            $geo = $response->json();
+
+            ProfileVisitor::create([
+                'ip' => $request->ip(),
+                'browser' => $request->header('User-Agent'),
+                'system' => php_uname(),
+                'country' => $geo['country'] ?? null,
+                'country_code' => $geo['countryCode'] ?? null,
+                'region' => $geo['regionName'] ?? null,
+                'city' => $geo['city'] ?? null,
+                'timezone' => $geo['timezone'] ?? null,
+                'lat' => is_numeric($geo['lat'] ?? null) ? floatval($geo['lat']) : null,
+                'lng' => is_numeric($geo['lon'] ?? null) ? floatval($geo['lon']) : null,
+                'isp' => $geo['isp'] ?? null,
+                'organization' => $geo['org'] ?? null,
+                'guid' => \Str::uuid(),
+                'page_type' => 'home',
+            ]);
+        }
         return view('user.home');
     }
 
